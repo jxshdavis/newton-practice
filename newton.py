e1fcd5108395ee89092f0d1df9e8024e6185e685
Josh Davis <78579945+jxshdavis@users.noreply.github.com>
2025-08-21 11:13:37 -0700

    Fixed the multivariate method, formated with ruff, got a working example of multivar newtons method.

--- newton.py
+++ newton.py
@@ -1,5 +1,5 @@
 import math
-from scipy.differentiate import hessian, derivative
+import numdifftools as nd
 import numpy as np
 
 
@@ -18,13 +18,13 @@ def approximate_derivative(func, value, epsilon):
 
 
 def multivar_newtons_method(
-    function, starting_value=0, epsilon=10 ** (-5), tolerance=10 ** (-5)
+    function, starting_value, epsilon=10 ** (-5), tolerance=10 ** (-5)
 ):
     """An approximate implementation of Newton's method for univariate functions.
 
-    Args: 
+    Args:
         function (function): Function we are optimizing.
-        starting_value (int, optional): Starting point for the optimization procedure. Defaults to 0.
+        starting_value (np.ndarray, optional): Starting point for the optimization procedure
         epsilon (float, optional): _description_. Step size for approximating the frist and second derivatives. Defaults to 10**(-3).
         tolerance (float, optional): _description_. How small the change in x_t-x_{t-1} must be in order to stop procedure. Defaults to 10**(-5).
 
@@ -37,35 +37,30 @@ def multivar_newtons_method(
     # handliing the case where starting value is not a real number, or numpy array
     if not isinstance(starting_value, (int, float, np.ndarray)):
         raise TypeError(
-            f"Starting value must be a number, or numpy array, tuple, list it is of type {type(starting_value)}"
+            f"Starting value must be a numpy ndarray it is of type {type(starting_value)}"
         )
 
     step = tolerance + 1
     current_val = starting_value
 
     while step > tolerance:
-        gradient = derivative(function, current_val, tolerances={"atol":tolerance})
-        hess = hessian(function, current_val)
+        grad = nd.Gradient(function, step=epsilon)(current_val)
+        hess = nd.Hessian(function, step=epsilon)(current_val)
         # check that the hessian has large enough determinant
         if np.linalg.det(hess) < 1e-7:
             raise RuntimeError(
                 "Determinant of Hessian is too close to zero, Newton's method may diverge."
             )
 
-        next_val = current_val - np.linalg.inv(hess) @ gradient
-        step = np.sum((next_val - current_val)**2)**(1/2)
+        next_val = current_val - np.linalg.inv(hess) @ grad
+        step = np.sum((next_val - current_val) ** 2) ** (1 / 2)
         current_val = next_val
 
-
-
-    print(
-        f"A function critical value occurs near the interval ({current_val})!"
-    )
+    print(f"A function critical value occurs near the interval ({current_val})!")
     return {"x": current_val, "f(x)": function(current_val)}
 
 
-
-def newtons_method(f, starting_value, epsilon=10**(-5), tolerance=10**(-5)):
+def newtons_method(f, starting_value, epsilon=10 ** (-5), tolerance=10 ** (-5)):
     if not callable(function):
         raise TypeError(f"Argument is not a function, it is of type {type(function)}")
 
@@ -97,9 +92,6 @@ def newtons_method(f, starting_value, epsilon=10**(-5), tolerance=10**(-5)):
         optima = "minimum"
     elif second_derivative < 0:
         optima = "maximum"
-    
-
-
 
     print(
         f"A function {optima} occurs near the interval ({current_val - tolerance}, {current_val + tolerance})!"
@@ -107,15 +99,17 @@ def newtons_method(f, starting_value, epsilon=10**(-5), tolerance=10**(-5)):
     return {"x": current_val, "f(x)": function(current_val)}
 
 
-
-
 def test_multivar_newton():
     print("Testing on x^2+y^2 starting at (x,y)=(1,1)")
     print(
-         multivar_newtons_method(lambda x: x[0]**2 + x[1]**2, starting_value=np.array([1, 1]))
+        multivar_newtons_method(
+            lambda x: x[0] ** 2 + x[1] ** 2, starting_value=np.array([1, 1])
         )
+    )
+
 
 test_multivar_newton()
+
 
 def test_newton():
     print("Testing on x^2 starting at x=2.")
@@ -130,9 +124,6 @@ def test_newton():
     print(
         newtons_method(lambda x: math.sin(math.sin(x) + math.cos(x)), starting_value=1)
     )
-
-
-
 
 
 # print("Testing on x**4/4-x**3-x starting at x=1.")
